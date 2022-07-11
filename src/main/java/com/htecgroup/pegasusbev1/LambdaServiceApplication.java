ad1df007696d53a5e89276e62cd285b06a74381d
bojank123 <bojan.kozomara@htecgroup.com>
2022-07-11 10:12:20 +0200

    mrs

---
+++ src/main/java/com/htecgroup/pegasusbev1/LambdaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.htecgroup.pegasusbev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LambdaServiceApplication.class, args);
+  }
+
+}
